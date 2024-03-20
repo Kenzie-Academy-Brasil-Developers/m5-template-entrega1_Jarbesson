98a01cbef23e1a91db3fa1f1cbf0038196f9930e
Jarbesson <135034051+Jarbesson@users.noreply.github.com>
2024-03-20 17:43:53 -0400

    rs

--- dist/src/schemas/session.schema.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sessionCretaSchema = void 0;
-const user_schema_1 = require("./user.schema");
-exports.sessionCretaSchema = user_schema_1.userSchema.pick({ email: true, password: true });
